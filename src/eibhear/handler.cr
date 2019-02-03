d75c7c76a5802e46b16bd22e9ceb2052a6109164
Samual Black <samuallb@hotmail.com>
2019-02-03 13:59:57 +0000

    Removed Citrine-i18n dependency

--- src/eibhear/handler.cr
+++ src/eibhear/handler.cr
@@ -3,17 +3,42 @@ require "citrine-i18n"
 
 class Eibhear::Handler
   include HTTP::Handler
+  HEADER = "Accept-Language"
 
   def call(context)
     if languages = context.request.headers[Citrine::I18n::Handler::HEADER]?
       parser = Citrine::I18n::Parser.new languages
       old = context.locales.clone
-      context.locales.clear    
-      parser.user_preferred_languages.each do |loc|
+      context.locales.clear
+      headers = context.request.headers[HEADER]
+      parse_languages(headers).each do |loc|
         context.locales << loc if Eibhear.config.available_locales.includes?(loc)
       end
       context.locales = old if context.locales.empty?
     end
     call_next(context)
   end
+
+  # Taken from citrine-i18n/parser.cr:23
+  def self.parse_languages(header)
+    begin
+      header.to_s.gsub(/\s+/, "").split(",").map do |language|
+        split = language.split(";q=")
+        locale, quality = split[0], split[1]?
+        raise ArgumentError.new "Not correctly formatted" unless locale =~ /^[a-z\-0-9]+|\*$/i
+
+        locale = locale.downcase.gsub(/-[a-z0-9]+$/i, &.upcase)
+        locale = nil if locale == "*"
+
+        quality = quality ? quality.to_f : 1.0
+
+        {locale, quality}
+      end.sort do |(_, left), (_, right)|
+        right <=> left
+      end.map(&.first).compact
+    rescue ArgumentError
+      [] of String
+    end
+  end
 end
+g
